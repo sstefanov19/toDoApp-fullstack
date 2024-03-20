d0123e349c7380b6f262d31e3b517cb691021387
xdStefcho <84671344+xdStefcho@users.noreply.github.com>
2024-03-20 23:16:04 +0200

    initial

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds  = 10;
+env.config();
 
 const db = new pg.Client({
   user: "postgres",
@@ -24,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret : 'idk',
+  secret : process.env.SESSION_SECRET,
   resave : false,
   saveUninitialized : true,
   cookie : {
